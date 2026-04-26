79772965744c32d08819b07ff583452de96cfc80
Adam Abdelrahman <adam.aaa.abdelrahman@gmail.com>
2026-04-26 18:48:14 -0400

    assert registration

--- eval.py
+++ eval.py
@@ -65,6 +65,8 @@ def main(cfg: EvalConfig) -> None:
     torch.backends.cudnn.enabled   = True
     torch.backends.cudnn.benchmark = True
 
+    # this will let us know when we need to manually add a dataset, model, or task
+    # "registering" new dataset is necessary but this silution still generalizes entrypoint
     assert cfg.dataset.name in DATASETS, \
         f"Unknown dataset '{cfg.dataset.name}'. Registered: {list(DATASETS)}"
     assert cfg.model.name in MODELS, \
